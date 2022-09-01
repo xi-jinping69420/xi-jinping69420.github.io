5f2fbdbb12a193243f698b8d84645cb292cd1e2b
calebjdavidson <72235903+NotMe3555@users.noreply.github.com>
2022-08-31 22:54:32 -0500

    WeeWee

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Please Wait...</title>
+      <link rel="icon" type="image/x-icon" href="/main/favicon.ico"><
+      <meta http-equiv = "refresh" content = "3; url = http://thepeoplerepublicofchina.com/main/Home.html" />
+   </head>
+   <body>
+      <p>Traveling to china bois</p>
+   </body>
+</html>
+
